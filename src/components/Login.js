02ba811a7ca9b86f503e497979af7bc2e7aa10a0
bdb2381 <brad.beggs@gmail.com>
2020-10-20 22:24:48 -0700

    styled login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,38 +32,39 @@ class Login extends React.Component {
   render() {
     return (
       // <div className="one-column-grid">
-      <div>
+      <div className="login-container">
 
-          <h1> Ruby Rentals - All The Best Gear For Adventure.  </h1>
-          <h2>Log in below:</h2>
+          <h1>All The Best Gear For Adventure.  </h1>
+          <h2>Log in below</h2>
 
         <form onSubmit={(e) => this.handleLoginSubmit(e)}>
           
-          <div className="profile-form-row-center">
-            <label name="email">Email</label>
-            <input
-              name="email"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={(e) => this.handleLoginChange(e)}
-            ></input>
+          <div className="login-form-center">
+           <div>
+              <input
+              type="text"
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={(e) => this.handleLoginChange(e)}
+              ></input>
+            </div>
+            <div>       
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={(e) => this.handleLoginChange(e)}>
+              </input>
+            </div>
           </div>
-
-          <div className="profile-form-row-center">
-            <label name="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={this.state.password}
-              onChange={(e) => this.handleLoginChange(e)}
-            ></input>
-          </div>
-
-          <button>Submit</button>
+            <div>
+              <button className="loginButton">Log in</button>
+            </div>
         </form>
 
-        <div>
+        <div className="create-account-link">
           <Link to="/signup"  className="signup-link">
             New? Create an account.
           </Link>
