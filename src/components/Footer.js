2b7785c518ee07210c456f689f5777878da4e216
bdb2381 <brad.beggs@gmail.com>
2020-10-20 22:27:05 -0700

    add margin to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Footer = () => {
 
 return(
-<>
-<div>
+<div className="footer">
+<div >
    2020 Ruby Rentals, Inc. All rights reserved. Ruby and the Ruby wordmark are trademarks of Ruby Rentals Equipment, Inc.
    </div>
    <div>
   Terms of Use | Privacy Policy - UPDATED Interest Based Ads Product | Recalls | CA Transparency Act |  Accessibility Statement |
   </div>
-</>
+  </div>
 
   )
 
